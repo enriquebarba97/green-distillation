09ca141ebeb5ba7cb16321a1bf5428f0186ecfa3
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2025-06-23 12:20:26 +0200

    Update surrogate.sh

--- surrogate.sh
+++ surrogate.sh
@@ -2,7 +2,7 @@
 for i in {70..79}
 do
     echo "Running surrogate model for iteration $i"
-    CMD="docker run -a stdout --gpus all --rm -v $(pwd):/root/green green_env /bin/bash -c 'cd /root/green/CodeT5+/Summarization/; python3 create_surrogate_model.py --start-from $i --single"
+    CMD="docker run -a stdout --gpus all --rm -v $(pwd):/root/green green_env /bin/bash -c 'cd /root/green/CodeT5+/Summarization/; python3 create_surrogate_model.py --start-from $i --single'"
 
     eval $CMD
 
