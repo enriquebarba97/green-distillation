7892f5bf1ff160a71705cb82fe49ac43bb028b77
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2025-08-03 19:01:57 +0200

    VD: Create testing models with FLOPS and energy

---
+++ create_morph_models.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MODELS=20
+
+NAME="GCB-Vulnerability-Detection"
+TASK_PATH="GraphCodeBERT/Vulnerability-Detection/Morph"
+
+for ((i = 0; i < MODELS; i++)); do
+    echo "Running model $i"
+    CMD="docker run --gpus all --rm -a stdout -v ./:/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin'"
+done
+
+for ((i = 0; i < MODELS; i++)); do
+    echo "Running model $i"
+    CMD="docker run --gpus all --rm -a stdout -v ./:/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin --use-flops'"
+done
