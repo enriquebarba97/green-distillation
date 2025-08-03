0b0200b84382071a2726565d99253451d0cb028e
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2025-08-03 19:04:57 +0200

    Update create_morph_models.sh

--- create_morph_models.sh
+++ create_morph_models.sh
@@ -6,10 +6,10 @@ TASK_PATH="GraphCodeBERT/Vulnerability-Detection/Morph"
 
 for ((i = 0; i < MODELS; i++)); do
     echo "Running model $i"
-    CMD="docker run --gpus all --rm -a stdout -v ./:/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin'"
+    CMD="docker run --gpus all --rm -a stdout -v $(pwd):/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin'"
 done
 
 for ((i = 0; i < MODELS; i++)); do
     echo "Running model $i"
-    CMD="docker run --gpus all --rm -a stdout -v ./:/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin --use-flops'"
+    CMD="docker run --gpus all --rm -a stdout -v $(pwd):/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin --use-flops'"
 done
