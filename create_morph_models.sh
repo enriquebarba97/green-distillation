2d3e20be48df5a47aeaf2d5accf542c6d80e8bce
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2025-08-03 19:26:35 +0200

    Update create_morph_models.sh

--- create_morph_models.sh
+++ create_morph_models.sh
@@ -4,6 +4,14 @@ MODELS=20
 NAME="GCB-Vulnerability-Detection"
 TASK_PATH="GraphCodeBERT/Vulnerability-Detection/Morph"
 
+# Trap sigint, stop docker and break the loop
+function cleanup {
+    echo "Stopping Docker containers..."
+    docker stop $(docker ps -q)
+    exit 0
+}
+trap cleanup SIGTERM SIGINT
+
 for ((i = 0; i < MODELS; i++)); do
     echo "Running model $i"
     CMD="docker run --gpus all --rm -a stdout -v $(pwd):/root/green green_env /bin/bash -c 'cd /root/green/${TASK_PATH}; python3 many_objective.py --model-name model-$i.bin'"
