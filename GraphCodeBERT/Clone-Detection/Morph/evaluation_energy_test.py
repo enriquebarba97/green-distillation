ebd2d0d3909510605fdffdb64cbc37a83832272d
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2025-08-29 19:07:11 +0200

    Repeat surrogate measurements CD

--- GraphCodeBERT/Clone-Detection/Morph/evaluation_energy_test.py
+++ GraphCodeBERT/Clone-Detection/Morph/evaluation_energy_test.py
@@ -14,6 +14,8 @@ def main():
                         help="Model to test energy performance.")
     parser.add_argument("--use-flops", action='store_true',
                         help="Take models from FLOPs optimization instead of energy optimization.")
+    parser.add_argument("--pareto", action='store_true',
+                        help="Use Pareto front models for RQ1")
     parser.add_argument("--surrogate", action='store_true',
                         help="Measuring energy of surrogate models.")
     args = parser.parse_args()
@@ -24,16 +26,18 @@ def main():
 
     if args.surrogate:
         # Load surrogate data
-        hyperparams_data = pd.read_csv('surrogate_data_metamorphic.csv').to_numpy()
-        num_objs = 2
+        hyperparams_data = pd.read_csv('surrogate_data_metamorphic_NEW.csv').to_numpy()
+        num_objs = 3
     # Load Pareto front data and convert to array
+    if args.pareto:
+        hyperparams_data = pd.read_csv('mo_pareto_front_rq1.csv').to_numpy()
+        num_objs = 5
     elif args.use_flops:
         hyperparams_data = pd.read_csv('morph_results_flops.csv').to_numpy()
-        num_objs = 4
+        num_objs = 5
     else:
         hyperparams_data = pd.read_csv('morph_results_energy.csv').to_numpy()
-        num_objs = 4
-
+        num_objs = 5
 
     hyperparameters = hyperparams_data[:, :hyperparams_data.shape[1] - num_objs] # 2 for surrogate, 4 for final models
     objectives = hyperparams_data[:, hyperparams_data.shape[1] - num_objs:]
@@ -44,8 +48,16 @@ def main():
     hyperparams = hyperparameters[model_number]
     objs = objectives[model_number]
 
-    eval_rounds = 4
-    accs, _ = distill([hyperparams], eval=True, surrogate=args.surrogate, seed=seed, model_name=f"model-{model_number}.bin", eval_rounds=eval_rounds, use_flops=args.use_flops)
+    # RQ1: Pareto measurements
+    if args.pareto:
+        model_name = f"pareto_{model_number}.bin"
+    else:
+        # RQ2: Final models
+        model_name = f"model-{model_number}.bin"
+
+
+    eval_rounds = 6
+    accs, _ = distill([hyperparams], eval=True, surrogate=False, seed=seed, model_name=model_name, eval_rounds=eval_rounds, use_flops=args.use_flops)
 
     print(accs)
 
